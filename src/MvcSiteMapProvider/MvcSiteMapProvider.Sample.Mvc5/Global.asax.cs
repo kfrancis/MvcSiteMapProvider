e69a4da29ca70a45b47c26d5fdb8b17486014ffe
Kori Francis <kfrancis@clinicalsupportsystems.com>
2025-09-30 10:50:22 -0400

    Integrate SimpleInjector DI with MvcSiteMapProvider
    Added SimpleInjector-based DI setup for MvcSiteMapProvider in the sample MVC5 project, including new DI modules, configuration, and schema files. Updated project references, configuration files, and startup logic to support external DI container usage and enable sitemap validation and route registration via SimpleInjector.

--- src/MvcSiteMapProvider/MvcSiteMapProvider.Sample.Mvc5/Global.asax.cs
+++ src/MvcSiteMapProvider/MvcSiteMapProvider.Sample.Mvc5/Global.asax.cs
@@ -1,10 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Reflection;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MvcSiteMapProvider.Loader;
+using MvcSiteMapProvider.Web.Mvc;
+using MvcSiteMapProvider.Xml;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
 
 namespace MvcSiteMapProvider.Sample.Mvc5
 {
@@ -12,6 +21,22 @@ namespace MvcSiteMapProvider.Sample.Mvc5
     {
         protected void Application_Start()
         {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            // This is an extension method from the integration package.
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+
+            // Setup global sitemap loader (required)
+            SiteMaps.Loader = container.GetInstance<ISiteMapLoader>();
+            var validator = container.GetInstance<ISiteMapXmlValidator>();
+            validator.ValidateXml(HostingEnvironment.MapPath("~/Mvc.sitemap"));
+            XmlSiteMapController.RegisterRoutes(RouteTable.Routes);
+
+            container.Verify();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
